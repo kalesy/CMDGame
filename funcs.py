55d0c7ad0aaedd29316db4d517333cf203770aa1
akatuki <borderofextasy@gmail.com>
2020-10-26 00:34:42 +0800

    implemetaion of leveling up

--- funcs.py
+++ funcs.py
@@ -2,29 +2,39 @@ import random as r
 class unit:
     def __init__(self, level, hp, attack, block):
         self.level = level
-        self.hp = hp
+        self.hp = self.maxhp = hp
         self.damageTaken = 0
         self.attack = attack
         self.block = block
-    def MaxHp(self):
-        return self.hp
     def HP(self):
-        return self.hp - self.damageTaken
+        return self.hp
     def IsDefeated(self):
-        return self.damageTaken >= self.hp
+        return self.hp < 0
     def TakeDamage(self, damage):
-        self.damageTaken += damage - self.block 
+        self.hp -= damage - self.block 
 
 class Character(unit): 
     def __init__(self):
-        unit.__init__(self, 0, 100, 5, 0)
+        unit.__init__(self, 1, 100, 5, 0)
+        print('Initialization complete, You are a hero of %d Hp and %d Attack'%(self.hp, self.attack))
         self.exp = 0
     def GainExp(self, unit):
-        i = unit.MaxHp() / self.MaxHp() * 100 
+        i = unit.maxhp / self.maxhp * 100 
         self.exp += i
-        print('You gain %d exp of total %d' % (i, self.exp))
-    def LevelUp(self):
+        print('You gain %d exp of total %d requiring %d' % (i, self.exp, self.__levelRequire()))
+        if(self.exp > self.__levelRequire()):
+            self.__levelUp()
+    def __levelUp(self):
         self.level += 1
+        self.hp = self.maxhp + self.__levelUpRandom(1, 5)
+        self.maxhp = self.hp
+        self.attack = self.attack + self.__levelUpRandom(1, 3)
+        print('Congratulations! Level up to %d of %d hp and %d attack'%(self.level, self.hp, self.attack))
+    def __levelUpRandom(self, min, max):
+        return r.randint(min, max)
+
+    def __levelRequire(self):
+        return self.level * 100 * 0.8
 
 class EnemeyFactory:
     def __init__(self, EnemyCount):
