3a09039814534976a16539e4815f629adb29ddf7
kalesy <285361731@qq.com>
2024-10-31 22:50:06 +0800

    add type annotations

--- funcs.py
+++ funcs.py
@@ -1,44 +1,52 @@
 import random as r
-class unit:
-    def __init__(self, level, hp, attack, block):
+class Unit:
+    def __init__(self, level:int, hp:int, attack:int, block:int):
         self.level = level
+        self.maxhp:int
         self.hp = self.maxhp = hp
         self.damageTaken = 0
         self.attack = attack
         self.block = block
+
     def HP(self):
         return self.hp
-    def IsDefeated(self):
+
+    def IsDefeated(self) -> bool:
         return self.hp < 0
-    def TakeDamage(self, damage):
+
+    def TakeDamage(self, damage:int):
         self.hp -= damage - self.block 
 
-class Character(unit): 
+class Character(Unit): 
+
     def __init__(self):
-        unit.__init__(self, 1, 100, 5, 0)
+        super().__init__(1, 100, 5, 0)
         print('Initialization complete, You are a hero of %d Hp and %d Attack'%(self.hp, self.attack))
         self.exp = 0
-    def GainExp(self, unit):
+
+    def GainExp(self, unit:Unit):
         i = unit.maxhp / self.maxhp * 100 
         self.exp += i
         print('You gain %d exp of total %d requiring %d' % (i, self.exp, self.__levelRequire()))
         if(self.exp > self.__levelRequire()):
             self.__levelUp()
+
     def __levelUp(self):
         self.level += 1
         self.hp = self.maxhp + self.__levelUpRandom(1, 5)
         self.maxhp = self.hp
         self.attack = self.attack + self.__levelUpRandom(1, 3)
         print('Congratulations! Level up to %d of %d hp and %d attack'%(self.level, self.hp, self.attack))
-    def __levelUpRandom(self, min, max):
+
+    def __levelUpRandom(self, min:int, max:int) -> int:
         return r.randint(min, max)
 
     def __levelRequire(self):
         return self.level * 100 * 0.8
 
 class EnemeyFactory:
-    def __init__(self, EnemyCount):
-        self.array = [unit(r.randint(1,5),r.randint(20,50), r.randint(2,7), 0) for x in range(0, EnemyCount)]
+    def __init__(self, EnemyCount:int):
+        self.array = [Unit(r.randint(1,5),r.randint(20,50), r.randint(2,7), 0) for x in range(0, EnemyCount)]
         self.currentEnemyIndex = -1
     def GetEnemy(self):
         l = len(self.array)
@@ -48,6 +56,6 @@ class EnemeyFactory:
             self.currentEnemyIndex = r.randint(0, len(self.array) - 1)
             return self.array[self.currentEnemyIndex]
     def EnemeyDefeat(self):
-        self.array.pop(self.currentEnemyIndex)
+        _ = self.array.pop(self.currentEnemyIndex)
     def EnemyCountLeft(self):
         return len(self.array)
